bef0207f1456e17abcebe3a241afc43117c0ae9f
osamakhalil98 <mozzbozz35@gmail.com>
2020-06-24 06:59:13 +0200

    :boom: added dark mode

---
+++ index.js
@@ -0,0 +1,33 @@
+const darkIcon = document.getElementById("icon-dark");
+const lisghting = document.querySelector(".lighting");
+const name = document.querySelector(".name");
+const anchors = document.querySelectorAll("a");
+let dark = false;
+darkIcon.addEventListener("click", function () {
+  if (dark === false) {
+    dark = true;
+    darkIcon.style.color = "white";
+    lisghting.style.backgroundColor = "black";
+    document.body.style.cssText =
+      " margin: 0; border: 12px solid white; background-color:black";
+    name.style.color = "white";
+    for (let i = 0; i < anchors.length; i++) {
+      anchors[i].style.backgroundColor = "black";
+      anchors[i].style.border = "2px solid white";
+      anchors[i].style.color = "white";
+    }
+  } else {
+    dark = false;
+    darkIcon.style.color = "black";
+    lisghting.style.backgroundColor = "white";
+    document.body.style.cssText =
+      " margin: 0; border: 12px solid black; background-color:white";
+    name.style.color = "black";
+    for (let i = 0; i < anchors.length; i++) {
+      anchors[i].style.backgroundColor = "white";
+      anchors[i].style.border = "2px solid black";
+      anchors[i].style.color = "black";
+    }
+  }
+});
+console.log(darkIcon);
